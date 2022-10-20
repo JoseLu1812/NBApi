4b65ade738f4a4739edc00a13083b4edc36f13aa
Admin <hidalgo.najos22@triana.salesianos.edu>
2022-10-20 14:22:15 +0200

    Starting working on players-list and details

--- NBAProject/src/app/app.module.ts
+++ NBAProject/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayersListComponent } from './components/players/players-list/players-list.component';
+import { PlayersDetailsComponent } from './components/players/players-details/players-details.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayersListComponent,
+    PlayersDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
