644a4a2731694021d3e115a355c9c23a2bd0874d
josgilmar2 <josgilmar2@gmail.com>
2022-10-21 13:20:12 +0200

    starting working on teams-details

--- NBAProject/src/app/components/teams/teams-details/teams-details.component.ts
+++ NBAProject/src/app/components/teams/teams-details/teams-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Team } from 'src/app/interfaces/teams.interface';
+import { TeamsService } from 'src/app/services/teams.service';
 
 @Component({
   selector: 'app-teams-details',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamsDetailsComponent implements OnInit {
 
-  constructor() { }
+  team: Team = {} as Team;
+  year: number = 0;
+  urlName: string = '';
+  teamList: Team[] = [];
+
+  constructor(private teamsService: TeamsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((res) => {
+      this.year = res['year'];
+      this.urlName = res['urlName'];
+    })
+    this.getTeams();
+  }
+
+  getTeams() {
+    this.teamsService.getTeams(this.year).subscribe(res => {
+      this.teamList = res.league.standard;
+      for (let team of this.teamList) {
+        if(team.urlName == this.urlName) {
+          this.team = team;
+        }
+      }
+    })
+  }
+
+  showTeamImages(t: Team) {
+    return `https://cdn.nba.com/logos/nba/${t.teamId}/global/L/logo.svg`;
   }
 
 }
