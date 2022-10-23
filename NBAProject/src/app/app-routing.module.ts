1cb803ed6fdc822a8905d10e2435a933c8dc348a
José Luis Hidalgo Navas <jhidalgonavas@gmail.com>
2022-10-23 21:33:18 +0200

    Merge branch '3-players'

--- NBAProject/src/app/app-routing.module.ts
+++ NBAProject/src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { PlayersListComponent } from './components/players/players-list/players-
 import { InitialPageComponent } from './components/initial-page/initial-page.component';
 import { TeamsListComponent } from './components/teams/teams-list/teams-list.component';
 import { PlayersDetailsComponent } from './components/players/players-details/players-details.component';
+import { TeamsDetailsComponent } from './components/teams/teams-details/teams-details.component';
+import { TeamsRosterComponent } from './components/teams/teams-roster/teams-roster.component';
 
 const routes: Routes = [
   {path: 'players-list', component: PlayersListComponent },
@@ -12,6 +14,7 @@ const routes: Routes = [
   {path: 'initial-page', component: InitialPageComponent},
   {path: 'teams-list', component: TeamsListComponent},
   {path: 'player-details/:year/:personId', component: PlayersDetailsComponent}
+  {path: 'team-details/:year/:urlName', component: TeamsDetailsComponent}
 ];
 
 @NgModule({
