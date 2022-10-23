278f127d8cefd6ca212c50b7402924f017468f72
José Luis Hidalgo Navas <jhidalgonavas@gmail.com>
2022-10-23 21:27:23 +0200

    no sirve se resetea

--- NBAProject/src/app/app-routing.module.ts
+++ NBAProject/src/app/app-routing.module.ts
@@ -4,14 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { PlayersListComponent } from './components/players/players-list/players-list.component';
 import { InitialPageComponent } from './components/initial-page/initial-page.component';
 import { TeamsListComponent } from './components/teams/teams-list/teams-list.component';
+<<<<<<< HEAD
 import { PlayersDetailsComponent } from './components/players/players-details/players-details.component';
+=======
+import { TeamsDetailsComponent } from './components/teams/teams-details/teams-details.component';
+>>>>>>> main
 
 const routes: Routes = [
   {path: 'players-list', component: PlayersListComponent },
   {path: '', redirectTo: '/initial-page', pathMatch: 'full'},
   {path: 'initial-page', component: InitialPageComponent},
   {path: 'teams-list', component: TeamsListComponent},
+<<<<<<< HEAD
   {path: 'player-details/:year/:personId', component: PlayersDetailsComponent}
+=======
+  {path: 'team-details/:year/:urlName', component: TeamsDetailsComponent}
+>>>>>>> main
 ];
 
 @NgModule({
