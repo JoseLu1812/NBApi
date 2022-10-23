d5789acdc862eec8c40990788b3ceceac9c66ca6
José Luis Hidalgo Navas <jhidalgonavas@gmail.com>
2022-10-23 21:33:47 +0200

    coma puesta

--- NBAProject/src/app/app-routing.module.ts
+++ NBAProject/src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   {path: '', redirectTo: '/initial-page', pathMatch: 'full'},
   {path: 'initial-page', component: InitialPageComponent},
   {path: 'teams-list', component: TeamsListComponent},
-  {path: 'player-details/:year/:personId', component: PlayersDetailsComponent}
+  {path: 'player-details/:year/:personId', component: PlayersDetailsComponent},
   {path: 'team-details/:year/:urlName', component: TeamsDetailsComponent}
 ];
 
