caaa07834dfd11dc5859b2a9aa8919318c930a3c
Admin <hidalgo.najos22@triana.salesianos.edu>
2022-10-26 14:25:59 +0200

    project finish

--- NBAProject/src/app/components/coaches/coaches.component.ts
+++ NBAProject/src/app/components/coaches/coaches.component.ts
@@ -1,61 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Coach } from 'src/app/interfaces/coaches.interface';
-import { Team } from 'src/app/interfaces/teams.interface';
 import { CoachesService } from 'src/app/services/coaches.service';
 import { TeamsService } from 'src/app/services/teams.service';
 
 @Component({
   selector: 'app-coaches',
   templateUrl: './coaches.component.html',
-  styleUrls: ['./coaches.component.css']
+  styleUrls: ['./coaches.component.css'],
 })
 export class CoachesComponent implements OnInit {
-
   yearsData: number[] = [2022, 2021, 2020, 2019, 2018, 2017, 2016];
   year: number = 0;
-  teamOfCoach: Team = {} as Team;
-  coach: Coach = {} as Coach;
   coachList: Coach[] = [];
-  teamList: Team[] = [];
-  
+  coach: Coach = {} as Coach;
 
-  constructor(private coachesService: CoachesService, private teamService: TeamsService) { }
+  constructor(
+    private coachesService: CoachesService,
+    private teamService: TeamsService
+  ) {}
 
   ngOnInit(): void {
-    this.getCoachList(2022);
-    this.getCoachTeam();
+    this.getOneCoach(2022);
   }
 
-  public getCoachList(year: number){
-    this.coachesService.getCoachList(year).subscribe(resp => {
+  getOneCoach(year: number) {
+    this.coachesService.getCoachList(year).subscribe((resp) => {
       this.coachList = resp.league.standard;
       this.year = year;
-    })
-  }
-
-  getCoachs(){
-    for(let c of this.coachList){
-      if()){
-
+      for (let c of this.coachList) {
+        this.coach = c;
       }
-    }
+    });
   }
-
-  public getCoachTeam(){
-    this.teamService.getTeams(this.year).subscribe(resp => {
-      this.teamList = resp.league.standard;
-      for (let c of this.coachList) {     
-        for (let t of this.teamList) {
-          if (t.teamId == c.teamId) {
-            this.teamOfCoach = t;
-          }        
-        }
-      }
-    })
-  }
-
   showCoachImages(c: Coach) {
     return `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${c.personId}.png`;
   }
-
 }
